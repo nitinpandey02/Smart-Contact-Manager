09da3a2e152ddc7764e25e20e2b3d860c5a502ab
Ntin Pandey <np4364689@gmail.com>
2024-05-07 23:02:23 +0530

    new files added

--- SmartContactManager/src/main/java/com/example/contactmanager/SmartContactManager/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/example/contactmanager/SmartContactManager/controller/HomeController.java
@@ -2,32 +2,30 @@ package com.example.contactmanager.SmartContactManager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.contactmanager.SmartContactManager.dao.UserRepository;
 import com.example.contactmanager.SmartContactManager.entities.Contact;
 import com.example.contactmanager.SmartContactManager.entities.User;
 
+
 @Controller
 public class HomeController {
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		
-		  User user=new User();
-		  user.setName("Nitin");
-		  user.setEmail("nitin.pandey@inc.com");
-		  
-		  Contact contact=new Contact();
-		  user.getContacts().add(contact);
-		  
-		  userRepository.save(user);
-		return "working";
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title","Home-Smart Contact MAnager");
+		return "home";
 	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About-Smart Contact MAnager");
+		return "about";
+	}
+	
 
 }
